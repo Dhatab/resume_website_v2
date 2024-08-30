488a181cf185a74e962d863824ac119f9985924a
Dhatab <danhatab@gmail.com>
2024-08-30 13:19:46 -0400

    Final Fixes

--- scripts.js
+++ scripts.js
@@ -1,44 +1,36 @@
-/*=====menu icon navbar======*/
+/*========== menu icon navbar ==========*/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 };
-
-/*=====scroll section active link======*/
-let sections =  document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
-
+/*========== scroll sections active link ==========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
-  sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
-
-    if(top >= offset && top < offset + height) {
-      navlinks.forEach(links => {
-        links.classList.remove('active');
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-      });
-
-    };
-  });
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
 
 
 
-  /*=====sticky navbar======*/
-  let header = document.querySelector('.header');
-  document.addEventListener('DOMContentLoaded', () => {
-    let header = document.querySelector('.header');
-    header.classList.add('sticky');
-  });
+/*========== sticky navbar ==========*/
+let header = document.querySelector('.header');
+header.classList.toggle('sticky', window.scrollY > 100);
 
-  /*=====remove menu icon navbar when click scroll======*/
-  menuIcon.classList.remove('bx-x');
-  navbar.classList.remove('active');
+/*========== remove menu icon navbar when click navbar link (scroll) ==========*/
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
 
   /*=====initial sticky navbar======*/
@@ -48,46 +40,34 @@ window.onscroll = () => {
   });
 
 
-/*=====swiper======*/
+/*========== swiper ==========*/
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    loop: true,
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 1,
+  spaceBetween: 50,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-
-/*=====dark light mode======*/
+/*========== dark light mode ==========*/
 let darkModeIcon = document.querySelector('#darkMode-icon');
-
 darkModeIcon.onclick = () => {
   darkModeIcon.classList.toggle('bx-sun');
   document.body.classList.toggle('dark-mode');
 };
-
-/*=====modal======*/
-function openModal(imageSrc) {
-  var modal = document.getElementById('modal');
-  var modalImg = document.getElementById('modal-image');
-
-  // Set the source of the modal image to the provided image source
-  modalImg.src = imageSrc;
-
-  // Display the modal
-  modal.style.display = "block";
-}
-
-function closeModal() {
-  var modal = document.getElementById('modal');
-  modal.style.display = "none";
-}
+/*========== scroll reveal ==========*/
+ScrollReveal({
+  // reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
 
 
 
@@ -115,3 +95,63 @@ ScrollReveal().reveal('.home-content h1, .about-img img', {
 ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', {
   origin: 'right'
 });
+
+/*=====modal======*/
+function openModal(imageSrc) {
+  var modal = document.getElementById('modal');
+  var modalImg = document.getElementById('modal-image');
+
+  // Set the source of the modal image to the provided image source
+  modalImg.src = imageSrc;
+
+  // Display the modal
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  var modal = document.getElementById('modal');
+  modal.style.display = "none";
+}
+
+"use strict";
+
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+  let letters = word.textContent.split("");
+  word.textContent = "";
+  letters.forEach((letter) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.className = "letter";
+    word.append(span);
+  });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let rotateText = () => {
+  let currentWord = words[currentWordIndex];
+  let nextWord =
+    currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+  // rotate out letters of current word
+  Array.from(currentWord.children).forEach((letter, i) => {
+    setTimeout(() => {
+      letter.className = "letter out";
+    }, i * 80);
+  });
+  // reveal and rotate in letters of next word
+  nextWord.style.opacity = "1";
+  Array.from(nextWord.children).forEach((letter, i) => {
+    letter.className = "letter behind";
+    setTimeout(() => {
+      letter.className = "letter in";
+    }, 340 + i * 80);
+  });
+  currentWordIndex =
+    currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+rotateText();
+setInterval(rotateText, 4000);
